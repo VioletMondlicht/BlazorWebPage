2e02b429eba74ff79e0d285f79c37abf5a40c4e8
VioletMondlicht <160015434+VioletMondlicht@users.noreply.github.com>
2025-05-22 21:56:51 +0200

    fix unexpected error message

--- BlazorWebPage/BlazorWebPage/Program.cs
+++ BlazorWebPage/BlazorWebPage/Program.cs
@@ -21,7 +21,7 @@ services.AddScoped<ICalculatorService, CalculatorService>()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/* if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
 
@@ -32,7 +32,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+*/
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
